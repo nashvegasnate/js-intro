783126fd48f7883a92e81df4271766de6f01407d
Nathan Keith <nashvegasnate@gmail.com>
2021-01-16 11:58:11 -0600

    added functions

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Morning work for 1/16/21");
+// console.log("Morning work for 1/16/21");
+
+//function sayHello() {
+//console.log("Hello!");
+//}
+
+//const sayHello = () => {
+  //console.log("Hello!");
+//}
+
+//FUNCTIONS WITH ARGUMENTS
+//function logsHello(name) {
+  //console.log(`Hello, ${name}!`);
+//}
+//logsHello('Nathan K');
+const sum = 55;
+ const addsTwoNumbers = (num1, num2) => {
+   const sum = num1 + num2;
+   console.log("console log inside the function", sum);
+   return sum;
+ }
+
+ console.log(sum);
+ console.log(addsTwoNumbers(4, 4));
