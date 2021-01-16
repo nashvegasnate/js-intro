4d133ad17a89d52ce48171da05e987e7c4031e80
Nathan Keith <nashvegasnate@gmail.com>
2021-01-16 11:08:11 -0600

    added variables, data types and math

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 console.log("Morning work for 1/16/21");
+
+//VARIABLES
+/*
+let firstName = "Beyonce";
+let lastName = "Knowles";
+
+console.log(firstName + " " + lastName);
+console.log(`First Name: ${firstName} Last Name ${lastName}`);
+
+firstName = 'Nathan';
+lastName = 'Keith';
+console.log(firstName + " " + lastName);
+console.log(`First Name: ${firstName} Last Name ${lastName}`);
+
+let favoriteFood = "peanuts";
+
+console.log(favoriteFood);
+// console.log(favoriteFood)
+favoriteFood = "gumbo";
+
+console.log(favoriteFood);
+// console.log(favoriteFood)
+
+// END VARIABLES
+//START DATA TYPES
+-----accidentally deleted this section
+// END DATA TYPES
+
+*/
+//START MATH
+console.log(Math.pow(2, 2));
+
+console.log(Math.round(6.5));
+//output 7
+console.log(Math.round(6.45));
+//output 6
+console.log(Math.floor(6.999));
+//output 6
+console.log(Math.ceil(6.000001));
+//output 7
+
+const cat = 'kitty';
+console.log(cat.length);
+// END MATH
