79e8a80b1c42f239d8cf145e5c66bb9e0cdd9fac
Nathan Keith <nashvegasnate@gmail.com>
2021-01-16 13:09:48 -0600

    worked through conditional logic, etc)
    <

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 console.log("Morning work for 1/16/21");
+
+/*function thisIsTrue(arg1, arg2) {
+  if (arg1 !== "nice" && arg2 !== "Nice") {
+    
+  console.log(`You are ${arg1}`);
+} else {
+  console.log('false');
+}
+}
+
+ thisIsTrue("nice", "Nice");
+
+
+ function thisIsTrue(arg1, arg2) {
+  if (arg1.length < 3) {
+    
+  console.log(`You are ${arg1}`);
+} else if (arg2.length > 8) {
+  console.log(`You are ${arg1}`);
+} else {
+  console.log("You are nothing!");
+}
+}
+thisIsTrue("nothing, "mickeymouse");
+*/
+const canDrive = (age) => {
+  if (age >= 15) {
+    return true;
+  }
+  return false;
+}
+console.log(canDrive(13));
